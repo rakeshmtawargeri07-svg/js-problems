7be0b2f783e47aa230cba4b8aa49a8fc93758240
Rakesh <rakeshmtawargeri07@gmail.com>
2026-05-07 22:19:46 +0530

    pushing to github

---
+++ lunchproblem.js
@@ -0,0 +1,18 @@
+function whoWillPay(names) {
+   
+    const randomNumber = Math.random();
+
+    
+    
+    const randomIndex = Math.floor(randomNumber * names.length);
+
+    
+    const selectedPerson = names[randomIndex];
+
+    return `${selectedPerson} is going to buy the meal today!`;
+}
+
+const friends = ["Alice", "Bob", "Charlie", "David"];
+
+
+console.log(whoWillPay(friends));
