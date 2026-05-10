3c0a8eeaba20733db67f19e13ef49d5a5667c682
Rakesh <rakeshmtawargeri07@gmail.com>
2026-05-10 21:06:42 +0530

    lunch problem

---
+++ lunch.js
@@ -0,0 +1,16 @@
+var name=["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+
+function whosPaying(name) {
+    
+/******Don't change the code above*******/
+var selectpersonindex=Math.floor(Math.random()*name.length);
+var selectrandomperson=name[selectpersonindex];
+
+console.log(selectrandomperson+" is going to buy lunch today!");
+
+    
+    //Write your code here.
+
+
+/******Don't change the code below*******/    
+}
